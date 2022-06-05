7cccc766ad5d3cd9629b937996370f5857cac45d
Ajayakarki <ajaya.carkey890@gmail.com>
2022-06-05 15:26:24 +0545

    Restricting order details to customer

--- myproject/myshop/views.py
+++ myproject/myshop/views.py
@@ -253,6 +253,14 @@ class LogoutView(View):
 class CustomerProfileView(TemplateView):
     template_name = 'customer_profile.html'
 
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user.customer:
+            pass
+        else:
+            return redirect('login')
+
+        return super().dispatch(request, *args, **kwargs)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         customer = self.request.user.customer
@@ -267,6 +275,20 @@ class CustomerOrderDetailView(DetailView):
     template_name = 'customer_order.html'
     model = Order
     context_object_name = 'order_obj'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user.customer:
+            order_id = self.kwargs['pk']
+            print(order_id)
+            order = Order.objects.get(id=order_id)
+            if request.user != order.cart.customer:
+                return redirect('customer_profile')
+        else:
+            return redirect('login')
+
+        return super().dispatch(request, *args, **kwargs)
+
+   
 
     
 
